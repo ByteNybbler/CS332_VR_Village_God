504781d6b7407163fbc2177664156e890d8c8f21
TheHunterGolden <huntergolden0124@gmail.com>
2017-03-22 04:51:44 -0400

    added sun scene and script

---
+++ Assets/_Scenes/TestSun/sun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour {
+
+	public Transform center;
+	public float speed;
+
+	void Update () {
+		gameObject.transform.RotateAround (center.transform.position, Vector3.left, speed * Time.deltaTime);	
+	}
+}

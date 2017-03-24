693a94f679de513408487c019da1c8dd0b1c4e2a
TheHunterGolden <huntergolden0124@gmail.com>
2017-03-24 06:52:50 -0400

    Merge remote-tracking branch 'refs/remotes/origin/master' into HunterBranch
    # Conflicts:
    #	Assets/_Scenes/TestSun.unity
    #	Assets/_Scenes/TestSun.unity.meta
    #	Assets/_Scenes/TestSun/sun.cs
    #	Assets/_Scenes/TestSun/sun.cs.meta

---
+++ Assets/_Scenes/TestSun/Plantation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plantation : MonoBehaviour {
+
+
+
+	public GameObject meter;
+
+	void Start () {
+		Instantiate (meter, gameObject.transform.position, gameObject.transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

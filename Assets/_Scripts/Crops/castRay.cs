5148d4101e78824927a608888701e2cbe21e419a
Hunter Golden <huntergolden0124@gmail.com>
2017-03-31 17:41:20 -0400

    thunder!!!!

--- Assets/_Scripts/Crops/castRay.cs
+++ Assets/_Scripts/Crops/castRay.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class castRay : MonoBehaviour {
-	public GameObject shrine;
-	private Shrine shrineComponent;
-	private int distance = 100;
 	private RaycastHit RayCastData;
-	public GameObject crop;
 	private int layerMasks;
-	private int cropCount;
 	public Vector3 location;
 
 	void start (){
-		shrineComponent = shrine.GetComponent<Shrine> ();
-		cropCount = 0;
+
+	}
+
+	void Update (){
+		Debug.DrawRay (transform.position, transform.forward, Color.green);
+		if (Physics.Raycast (transform.position, transform.forward, out RayCastData, Mathf.Infinity, layerMasks)) {
+			Debug.Log ("Hit At point: " + RayCastData.point);
+			location = RayCastData.point;
+		}
 	}
 	private void Awake()
 	{
@@ -22,24 +24,5 @@ public class castRay : MonoBehaviour {
 		layerMasks = 1 << 8;
 	}
 
-	public void MakeFood ()
-	{
-	
-		Debug.DrawRay (transform.position, transform.forward, Color.green);
-		 	              
-		if ((shrineComponent.SpendPoints (10)) && (cropCount < 11)) {
-			if (Physics.Raycast (transform.position, transform.forward, out RayCastData, Mathf.Infinity, layerMasks)) {
-				Debug.Log ("Hit At point: " + RayCastData.point);
-				location = RayCastData.point;
-				GameObject cropInstance = Instantiate (crop, RayCastData.point, Quaternion.identity);
-				cropInstance.GetComponent<plantHealth> ().OnDeath += OnCropDeath;
-				cropCount++;
-			}
-		}
-	}
 
-	public void OnCropDeath()
-	{
-		cropCount--;
-	}
 }	

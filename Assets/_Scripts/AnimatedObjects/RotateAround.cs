333dfa0861ae40f4ce1ff918bc354d37d454acbd
TheHunterGolden <huntergolden0124@gmail.com>
2017-04-07 15:45:52 -0400

    waterwheel working eyyyyy

---
+++ Assets/_Scripts/AnimatedObjects/RotateAround.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+	//Script that rotates a gameObject around its origin
+
+	void Update () {
+		gameObject.transform.RotateAround (gameObject.transform.position, Vector3.right, 20 * Time.deltaTime);
+	}
+}

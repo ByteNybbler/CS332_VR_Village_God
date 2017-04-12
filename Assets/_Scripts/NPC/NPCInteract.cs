d3fb5aaee7beb930f341214311d86df6330e74cf
Paul Calande <paul.luap09@gmail.com>
2017-04-12 00:39:44 -0400

    remove rigidbody NPC memes

--- Assets/_Scripts/NPC/NPCInteract.cs
+++ Assets/_Scripts/NPC/NPCInteract.cs
@@ -1,6 +1,9 @@
 ﻿// Author(s): Paul Calande
 // Villager interaction script for VR functionality.
 
+// Comment out the following line to disable all rigidbody functionality.
+//#define NPC_USING_RIGIDBODY
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,28 +22,36 @@ public class NPCInteract : VRTK_InteractableObject
 
     // Component references.
     private NavMeshAgent agent;
+#if NPC_USING_RIGIDBODY
     private Rigidbody rb;
+#endif
 
     protected override void Awake()
     {
         base.Awake();
         agent = GetComponent<NavMeshAgent>();
+#if NPC_USING_RIGIDBODY
         rb = GetComponent<Rigidbody>();
         SetKinematicEnabled(true);
+#endif
     }
 
     public override void Grabbed(GameObject currentGrabbingObject)
     {
         base.Grabbed(currentGrabbingObject);
         SetAgentEnabled(false);
+#if NPC_USING_RIGIDBODY
         SetKinematicEnabled(true);
+#endif
         StopAllCoroutines();
     }
 
     public override void Ungrabbed(GameObject previousGrabbingObject)
     {
         base.Ungrabbed(previousGrabbingObject);
+#if NPC_USING_RIGIDBODY
         SetKinematicEnabled(false);
+#endif
         StartCoroutine(TryToEnableAgent());
     }
 
@@ -54,15 +65,17 @@ public class NPCInteract : VRTK_InteractableObject
         movementComponent.enabled = isEnabled;
     }
 
+#if NPC_USING_RIGIDBODY
     /// <summary>
     /// Set the rigidbody's kinematic state.
     /// </summary>
-    /// <param name="isEnabled"></param>
+    /// <param name="isEnabled">True to enable the rigidbody's kinematic flag.</param>
     private void SetKinematicEnabled(bool isEnabled)
     {
         rb.isKinematic = isEnabled;
         //rb.detectCollisions = !isEnabled;
     }
+#endif
 
     // Coroutine to try to turn the agent and NPC movement components back on.
     // The components will only be enabled if the agent is a certain distance from the navmesh.
@@ -75,7 +88,9 @@ public class NPCInteract : VRTK_InteractableObject
             {
                 transform.position = hit.position;
                 SetAgentEnabled(true);
+#if NPC_USING_RIGIDBODY
                 SetKinematicEnabled(true);
+#endif
                 break;
             }
             else

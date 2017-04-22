25c532bc20767633947bdcc037b95d74d3bbcb5c
Paul Calande <paul.luap09@gmail.com>
2017-04-21 21:54:02 -0400

    WaterColor.cs warning fix

--- Assets/_Scripts/AnimatedObjects/WaterColor.cs
+++ Assets/_Scripts/AnimatedObjects/WaterColor.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
+﻿// Author(s): Hunter Golden, Paul Calande
+// Sets the color of the water particles.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterColor : MonoBehaviour {
-
-
-	void Start () {
-		this.GetComponent<ParticleSystem> ().startColor = new Color (1, 0, 1, .5f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class WaterColor : MonoBehaviour
+{
+	private void Start()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        //ps.startColor = new Color (1, 0, 1, .5f);
+        ParticleSystem.MainModule dupe = ps.main;
+        dupe.startColor = new Color(1f, 0f, 1f, .5f);
+    }
 }

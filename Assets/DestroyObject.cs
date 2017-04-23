04c3b310498e92e1e8d82430d56898a35c393515
TheHunterGolden <huntergolden0124@gmail.com>
2017-04-23 14:45:57 -0400

    added destroy script to plant ui

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+	[Tooltip("The GameObject to be destroyed.")]
+	public GameObject objectToDestroy;
+
+	[Tooltip("The amount of time before the Object is destroyed")]
+	public int time;
+	// Use this for initialization
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy (objectToDestroy, time);
+	}
+}
